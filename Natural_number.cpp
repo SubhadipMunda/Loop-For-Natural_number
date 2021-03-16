fa6e561e01ec8f5a17d93c9b89d79dd8729d75c9
Subhadip Munda <45973958+SubhadipMunda@users.noreply.github.com>
2021-03-16 08:36:19 +0530

    Add files via upload

---
+++ Natural_number.cpp
@@ -0,0 +1,16 @@
+/*natural number print out as user gives the limit*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int a = 1; a <= n; a++)
+    {
+        cout << a << endl;
+    }
+
+    return 0;
+}
